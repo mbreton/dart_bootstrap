a094c20335ee2a0c1cdb2ff2d91afd0917caaaa6
Mathieu Breton <mbreton@xebia.fr>
2013-01-12 14:50:46 +0100

    Add Procfile file to heroku deployement Replace main file with a server implementation

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'dart:json';
+
+main() {
+  var server = new HttpServer();
+  var port = int.parse(Platform.environment['PORT']);
+  server.listen('0.0.0.0', port);
+  print('Server started on port: ${port}');
+
+  server.defaultRequestHandler = (HttpRequest request, HttpResponse response) {
+
+    var resp = JSON.stringify({
+      'Hello': 'DevFest West',
+      'Dart on Heroku': true,
+      'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
+      'Environment': Platform.environment}
+    );
+
+    response.headers.set(HttpHeaders.CONTENT_TYPE, 'application/json');
+    response.outputStream.writeString(resp);
+    response.outputStream.close();
+  };
+}
