cb81108fc5df5933f8fd7b135fbc98fe45012699
Mathieu Breton <mbreton@xebia.fr>
2013-02-10 22:39:20 +0100

    Add comment on main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
 import 'dart:json' as JSON;
 
+/**
+ * This source code come from Seth Ladd's blog
+ * http://blog.sethladd.com/2012/08/running-dart-in-cloud-with-heroku.html
+ */
 main() {
   HttpServer server = new HttpServer();
   int port = int.parse(Platform.environment['PORT']);
